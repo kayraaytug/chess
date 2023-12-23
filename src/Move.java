18902b78eb6fcff4162e690d65e12e1013620c92
kayraaytug <kayraaytug@outlook.com>
2023-12-23 15:08:17 +0300

    I'm lost at this point...

--- src/Move.java
+++ src/Move.java
@@ -1,6 +1,4 @@
 import Pieces.*;
-
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class Move {
@@ -12,25 +10,63 @@ public class Move {
     }
 
     private void generateVerticalMoves(Piece piece){
-        for (int i = 0; i<8; i++){
+        for (int i = piece.positionOnBoardY; i<8; i++){
             // Self tile
             if(i==piece.positionOnBoardY){
-                continue;
             }
             else {
-                this.possibleMoves.add(pieces[piece.positionOnBoardX][i]);
+                if (pieces[piece.positionOnBoardX][i].team != piece.team){
+                    this.possibleMoves.add(pieces[piece.positionOnBoardX][i]);
+                }
+
+                if(!(pieces[piece.positionOnBoardX][i] instanceof Empty)){
+                    break;
+                }
+            }
+        }
+        for (int i = piece.positionOnBoardY; i>=0; i--){
+            // Self tile
+            if(i==piece.positionOnBoardY){
+            }
+            else {
+                if (pieces[piece.positionOnBoardX][i].team != piece.team){
+                    this.possibleMoves.add(pieces[piece.positionOnBoardX][i]);
+                }
+                if(!(pieces[piece.positionOnBoardX][i] instanceof Empty)){
+                    break;
+                }
             }
         }
     }
 
     private void generateHorizontalMoves(Piece piece){
-        for (int i = 0; i<8; i++){
+        for (int i = piece.positionOnBoardX; i<8; i++){
             // Self tile
             if(i==piece.positionOnBoardX){
-                continue;
+
             }
             else {
-                this.possibleMoves.add(pieces[i][piece.positionOnBoardY]);
+                if (pieces[i][piece.positionOnBoardY].team != piece.team){
+                    this.possibleMoves.add(pieces[i][piece.positionOnBoardY]);
+                }
+
+                if(!(pieces[i][piece.positionOnBoardY] instanceof Empty)){
+                    break;
+                }
+            }
+        }
+        for (int i = piece.positionOnBoardX; i>=0; i--){
+            // Self tile
+            if(i==piece.positionOnBoardX){
+
+            }
+            else {
+                if (pieces[i][piece.positionOnBoardY].team != piece.team){
+                    this.possibleMoves.add(pieces[i][piece.positionOnBoardY]);
+                }
+                if(!(pieces[i][piece.positionOnBoardY] instanceof Empty)){
+                    break;
+                }
             }
         }
     }
@@ -39,45 +75,108 @@ public class Move {
 
         // Top-left diagonal
         for (int x = piece.positionOnBoardX - 1, y = piece.positionOnBoardY - 1; x >= 0 && y >= 0; x--, y--) {
-            possibleMoves.add(pieces[x][y]);
+            if (piece.team != pieces[x][y].team){
+                possibleMoves.add(pieces[x][y]);
+            }
+            if (!((pieces[x][y]) instanceof Empty)){
+                break;
+            }
         }
 
         // Top-right diagonal
         for (int x = piece.positionOnBoardX + 1, y = piece.positionOnBoardY - 1; x < 8 && y >= 0; x++, y--) {
-            possibleMoves.add(pieces[x][y]);
+            if (piece.team != pieces[x][y].team){
+                possibleMoves.add(pieces[x][y]);
+            }
+            if (!((pieces[x][y]) instanceof Empty)){
+                break;
+            }
         }
 
         // Bottom-left diagonal
         for (int x = piece.positionOnBoardX - 1, y = piece.positionOnBoardY + 1; x >= 0 && y < 8; x--, y++) {
-            possibleMoves.add(pieces[x][y]);
+            if (piece.team != pieces[x][y].team){
+                possibleMoves.add(pieces[x][y]);
+            }
+            if (!((pieces[x][y]) instanceof Empty)){
+                break;
+            }
         }
 
         // Bottom-right diagonal
         for (int x = piece.positionOnBoardX + 1, y = piece.positionOnBoardY + 1; x < 8 && y < 8; x++, y++) {
-            possibleMoves.add(pieces[x][y]);
+            if (piece.team != pieces[x][y].team){
+                possibleMoves.add(pieces[x][y]);
+            }
+            if (!((pieces[x][y]) instanceof Empty)){
+                break;
+            }
         }
     }
 
     public ArrayList<Piece> GenerateAllMoves(Piece piece){
-        var team = piece.team;
-        var pieceType = piece.getClass();
-        var moveLimit = piece.moveLimit;
 
         // Black pawns
         if (piece instanceof Pawn && piece.team == 'b'){
-            this.possibleMoves.clear();
-            for (int i = piece.positionOnBoardY+1; i<=piece.positionOnBoardY+moveLimit; i++){
-                this.possibleMoves.add(pieces[piece.positionOnBoardX][i]);
+
+            // If pawn not on initial square, reduce its move limit
+            if (piece.positionOnBoardY != 1){
+                piece.moveLimit = 1;
             }
+
+            this.possibleMoves.clear();
+            try{
+                for (int i = piece.positionOnBoardY+1; i<=piece.positionOnBoardY+piece.moveLimit; i++){
+                // Prevent vertical capturing
+                    if (pieces[piece.positionOnBoardX][i] instanceof Empty){
+                        this.possibleMoves.add(pieces[piece.positionOnBoardX][i]);
+                    }
+                }
+
+                // Allow diagonal capturing
+                if (!(pieces[piece.positionOnBoardX+1][piece.positionOnBoardY+1] instanceof Empty)){
+                    this.possibleMoves.add(pieces[piece.positionOnBoardX+1][piece.positionOnBoardY+1]);
+                }
+                if (!(pieces[piece.positionOnBoardX-1][piece.positionOnBoardY+1] instanceof Empty)){
+                    this.possibleMoves.add(pieces[piece.positionOnBoardX-1][piece.positionOnBoardY+1]);
+                }
+            }
+            catch (IndexOutOfBoundsException ignore){
+
+            }
+
             return this.possibleMoves;
         }
 
         // White pawns
         else if (piece instanceof Pawn && piece.team == 'w'){
-            this.possibleMoves.clear();
-            for (int i = piece.positionOnBoardY-1; i>=piece.positionOnBoardY-moveLimit; i--){
-                possibleMoves.add(pieces[piece.positionOnBoardX][i]);
+
+            // If pawn not on initial square, reduce its move limit
+            if (piece.positionOnBoardY != 6){
+                piece.moveLimit = 1;
             }
+
+            this.possibleMoves.clear();
+            try{
+                for (int i = piece.positionOnBoardY-1; i>=piece.positionOnBoardY-piece.moveLimit; i--){
+                    // Prevent vertical capturing
+                    if (pieces[piece.positionOnBoardX][i] instanceof Empty){
+                        this.possibleMoves.add(pieces[piece.positionOnBoardX][i]);
+                    }
+                }
+                // Allow diagonal capturing
+
+                if (!(pieces[piece.positionOnBoardX+1][piece.positionOnBoardY-1] instanceof Empty)){
+                    this.possibleMoves.add(pieces[piece.positionOnBoardX+1][piece.positionOnBoardY-1]);
+                }
+                if (!(pieces[piece.positionOnBoardX-1][piece.positionOnBoardY-1] instanceof Empty)){
+                    this.possibleMoves.add(pieces[piece.positionOnBoardX-1][piece.positionOnBoardY-1]);
+                }
+            }
+            catch (IndexOutOfBoundsException ignore){
+
+            }
+
             return this.possibleMoves;
         }
 
@@ -138,99 +237,43 @@ public class Move {
             if (piece.moveType.contains("DIAGONAL")){
                 generateDiagonalMoves(piece);
             }
-            DetectCollision(piece);
+            //DetectCollision(piece);
             return this.possibleMoves;
         }
     }
 
-    public void DetectCollision(Piece piece){
-        System.out.println("DetectCollision called");
-        if(piece.moveType.contains("DIAGONAL")){
-
-        }
-        if (piece.moveType.contains("HORIZONTAL")){
-            int x = piece.positionOnBoardX+1;
-            int y = piece.positionOnBoardY;
-            boolean flag = false;
-            while (x < 8){
-                if (flag){
-                    this.possibleMoves.remove(pieces[x][y]);
-                }
-                else if(!(pieces[x][y] instanceof Empty)){
-                    this.possibleMoves.remove(pieces[x][y]);
-                    flag = true;
-                }
-                x++;
-            }
-
-            x = piece.positionOnBoardX-1;
-            y = piece.positionOnBoardY;
-            flag = false;
-            while (x >= 0){
-                if (flag){
-                    this.possibleMoves.remove(pieces[x][y]);
-                }
-                else if(!(pieces[x][y] instanceof Empty)){
-                    this.possibleMoves.remove(pieces[x][y]);
-                    flag = true;
-                }
-                x--;
-            }
-        }
-        if(piece.moveType.contains("VERTICAL")){
-            int x = piece.positionOnBoardX;
-            int y = piece.positionOnBoardY+1;
-            boolean flag = false;
-            while (y < 8){
-                if (flag){
-                    this.possibleMoves.remove(pieces[x][y]);
-                }
-                else if(!(pieces[x][y] instanceof Empty)){
-                    this.possibleMoves.remove(pieces[x][y]);
-                    flag = true;
-                }
-                y++;
-            }
-
-            x = piece.positionOnBoardX;
-            y = piece.positionOnBoardY-1;
-            flag = false;
-            while (y >= 0){
-                if (flag){
-                    this.possibleMoves.remove(pieces[x][y]);
-                }
-                else if(!(pieces[x][y] instanceof Empty)){
-                    this.possibleMoves.remove(pieces[x][y]);
-                    flag = true;
-                }
-                y--;
-            }
-        }
-    }
-
     public void movePiece(Piece piece, Piece target){
-        var piece_x_index = piece.positionOnBoardX;
-        var piece_y_index = piece.positionOnBoardY;
-        var piece_pos_x = piece.posX;
-        var piece_pos_y = piece.posY;
-
-        var target_x_index = target.positionOnBoardX;
-        var target_y_index = target.positionOnBoardY;
-        var target_pos_x = target.posX;
-        var target_pos_y = target.posY;
-
-        piece.positionOnBoardX = target_x_index;
-        piece.positionOnBoardY = target_y_index;
-        piece.posX = target_pos_x;
-        piece.posY = target_pos_y;
-
-        target.positionOnBoardX = piece_x_index;
-        target.positionOnBoardY = piece_y_index;
-        target.posX = piece_pos_x;
-        target.posY = piece_pos_y;
-
-        pieces[target_x_index][target_y_index] = piece;
-        pieces[piece_x_index][piece_y_index] = target;
+        //var piece_x_index = piece.positionOnBoardX;
+        //var piece_y_index = piece.positionOnBoardY;
+        //var piece_pos_x = piece.posX;
+        //var piece_pos_y = piece.posY;
+//
+        //var target_x_index = target.positionOnBoardX;
+        //var target_y_index = target.positionOnBoardY;
+        //var target_pos_x = target.posX;
+        //var target_pos_y = target.posY;
+//
+        //piece.positionOnBoardX = target_x_index;
+        //piece.positionOnBoardY = target_y_index;
+        //piece.posX = target_pos_x;
+        //piece.posY = target_pos_y;
+//
+        //target.positionOnBoardX = piece_x_index;
+        //target.positionOnBoardY = piece_y_index;
+        //target.posX = piece_pos_x;
+        //target.posY = piece_pos_y;
+//
+        //pieces[target_x_index][target_y_index] = piece;
+        //pieces[piece_x_index][piece_y_index] = target;
+        Piece tempTile = new Empty(piece.posX, piece.posY, 'e');
+        tempTile.positionOnBoardX = piece.positionOnBoardX;
+        tempTile.positionOnBoardY = piece.positionOnBoardY;
+        piece.positionOnBoardX = target.positionOnBoardX;
+        piece.positionOnBoardY = target.positionOnBoardY;
+        piece.posX = target.posX;
+        piece.posY = target.posY;
+        pieces[target.positionOnBoardX][target.positionOnBoardY] = piece;
+        pieces[tempTile.positionOnBoardX][tempTile.positionOnBoardY] = tempTile;
 
 
     }
